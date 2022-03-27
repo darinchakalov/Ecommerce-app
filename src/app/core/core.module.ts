c3ada7d4f6dc5c37d1764f15f671f2e019e21031
darinchakalov <darinchakalov@hotmail.com>
2022-03-27 08:17:22 +0300

    header and nav

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatBadgeModule } from '@angular/material/badge';
+
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatMenuModule, MatBadgeModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
