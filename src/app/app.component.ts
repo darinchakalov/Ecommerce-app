4d26b52cca01c8bc371ff33472ff9a5d3e32dad7
darinchakalov <darinchakalov@hotmail.com>
2022-03-31 07:29:30 +0300

    loader if is authenticating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from './user/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  get isAuthenticating(): boolean {
+    return this.userService.user === undefined;
+  }
+
+  constructor(private userService: AuthService) {
+    this.userService.getUserInfo().subscribe({
+      error: () => {
+        this.userService.user = null;
+      },
+    });
+  }
+}
