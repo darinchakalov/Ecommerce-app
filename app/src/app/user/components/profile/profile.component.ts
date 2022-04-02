e9751f2f40a755048b353f1b8add698299cdefde
darinchaklov <darinchakalov@hotmail.com>
2022-04-02 23:58:10 +0300

    updated my profile page

--- app/src/app/user/components/profile/profile.component.ts
+++ app/src/app/user/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from 'src/app/products/services/product.service';
+import { IProduct } from 'src/app/shared/interfaces/product';
 import { IUser } from 'src/app/shared/interfaces/user';
 import { AuthService } from '../../services/auth.service';
 
@@ -9,12 +11,27 @@ import { AuthService } from '../../services/auth.service';
 })
 export class ProfileComponent {
   user: IUser | undefined;
+  products!: IProduct[];
 
-  constructor(private userService: AuthService) {
+  get productsCount(): number {
+    return this.products?.length;
+  }
+
+  constructor(
+    private userService: AuthService,
+    private productService: ProductService
+  ) {
     this.getUserDetails();
+    this.fetchMyProducts();
   }
 
   getUserDetails() {
     this.userService.getUserInfo().subscribe((user) => (this.user = user));
+  }
+
+  fetchMyProducts() {
+    this.productService
+      .getMyProducts()
+      .subscribe((products) => (this.products = products));
   }
 }
