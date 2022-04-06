f7905cb16233771fd24027e98c04ad6cfd735d3f
darinchakalov <darinchakalov@hotmail.com>
2022-04-06 15:29:47 +0300

    contacts module and header

---
+++ app/src/app/contacts/components/contact-header/contact-header.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContactHeaderComponent } from './contact-header.component';
+
+describe('ContactHeaderComponent', () => {
+  let component: ContactHeaderComponent;
+  let fixture: ComponentFixture<ContactHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ContactHeaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContactHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ app/src/app/contacts/components/contact-main/contact-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-main',
+  templateUrl: './contact-main.component.html',
+  styleUrls: ['./contact-main.component.css']
+})
+export class ContactMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
